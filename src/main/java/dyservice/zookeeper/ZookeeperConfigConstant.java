d1b28ce4e1ca5f819628d44ff9b376601674ef47
truman-loo <trumanlp@gmail.com>
2012-09-04 15:51:36 +0800

    Update src/main/java/dyservice/zookeeper/ZookeeperConfigConstant.java

--- src/main/java/dyservice/zookeeper/ZookeeperConfigConstant.java
+++ src/main/java/dyservice/zookeeper/ZookeeperConfigConstant.java
@@ -9,7 +9,7 @@ package dyservice.zookeeper;
  */
 public final class ZookeeperConfigConstant {
 
-	public static final String CONN_STRING = "10.224.95.207:2181,10.224.95.221:2181";
+	public static final String CONN_STRING = "localhost:2181";
 	
 	public static final int SESSION_TIMEOUT = 30 * 1000;
 }
